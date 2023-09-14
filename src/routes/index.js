5f493775c4de391d031be78eab9a6a42bf7a88d2
Prayoga Volantis <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2023-09-14 17:07:22 +0700

    Initial Commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,7 @@ const Stack = createStackNavigator();
 export default function Routes() {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator screenOptions={{headerShown: false}}>
                 <Stack.Screen name="Main" component={HomeTabs} />
                 <Stack.Screen name="Details" component={DetailsScreen}/>
             </Stack.Navigator>
--- src/routes/tabs.js
+++ src/routes/tabs.js
@@ -2,13 +2,42 @@ import React from "react";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import HomeScreen from "../views/home/HomeScreen";
 import FavoriteScreen from "../views/home/FavoriteScreen";
+import { Ionicons } from 'react-native-vector-icons';
 const Tab = createBottomTabNavigator();
 
 export default function HomeTabs() {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Favorite" component={FavoriteScreen}/>
+        <Tab.Navigator screenOptions={{
+            tabBarShowLabel: false,
+            headerShown: false,
+            tabBarStyle: {
+                position: 'absolute',
+                bottom: 25,
+                left: 20,
+                right: 20,
+                elevation: 0,
+                borderRadius: 15,
+                height: 50,
+                backgroundColor: '#000',
+            }
+        }}>
+            <Tab.Screen options={{
+                tabBarIcon: ({focused, color, size}) => (
+                    <Ionicons
+                        name="home"
+                        size={24}
+                        color={focused ? '#fff' : '#ccc'} />
+                )
+            }} name="Home" component={HomeScreen} />
+            <Tab.Screen
+                options={{
+                    tabBarIcon: ({focused, color, size}) => (
+                        <Ionicons
+                            name="heart"
+                            size={24}
+                            color={focused ? '#fff' : '#ccc'} />
+                    ),
+                }} name="Favorite" component={FavoriteScreen}/>
         </Tab.Navigator>
     )
 }
