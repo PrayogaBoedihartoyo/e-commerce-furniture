886cce7d21d9cf39338f92beebb513154637b995
Prayoga Volantis <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2023-09-16 16:32:54 +0700

    Initial Commit

--- src/views/home/HomeScreen.js
+++ src/views/home/HomeScreen.js
@@ -1,29 +1,47 @@
 import React from "react";
-import { Text, View } from 'react-native';
-import {Button, TextInput} from "react-native-paper";
+import { TextInput, View, StyleSheet } from 'react-native';
+import { Ionicons } from 'react-native-vector-icons';
+import { IconButton } from 'react-native-paper';
 
-function HomeScreen({navigation}) {
+function HomeScreen() {
     return (
-        <View style={{
-          flex: 1,
-          justifyContent: 'center',
-          gap: 20,
-          margin: 20
-        }}>
-            <Text>Home Screen</Text>
-          <TextInput label="Email"/>
-            <Button
-              mode="contained"
-              onPress={() =>
-                navigation.navigate('Details', {
-                  itemId: 40,
-                })
-              }
-            >
-                Button Paper
-            </Button>
+      <View style={styles.container}>
+        <View style={[styles.searchBar, styles.borderShadow]}>
+          <Ionicons name="search" size={12} color="black" />
+          <TextInput placeholder="search"></TextInput>
         </View>
+          <IconButton icon="menu"/>
+      </View>
     )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  searchBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 32,
+    borderColor: 'gray',
+    padding: 10,
+    gap: 10,
+    marginHorizontal: 5,
+    flex: 1,
+  },
+  borderShadow: {
+    borderRadius: 50,
+    shadowColor: 'gray',
+    shadowOffset: {width: 2, height: 1},
+    shadowOpacity: 5,
+    shadowRadius: 2,
+    elevation: 5,
+    borderTopWidth: 0,
+    borderLeftWidth: 0.2,
+    backgroundColor: 'white',
+  }
+});
 
 export default HomeScreen;
