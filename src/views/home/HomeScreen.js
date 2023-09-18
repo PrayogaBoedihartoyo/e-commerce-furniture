d1227004a74dd5b8fdf380b90d47073705bbd140
Prayoga Volantis <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2023-09-18 08:13:18 +0700

    Initial Commit

--- src/views/home/HomeScreen.js
+++ src/views/home/HomeScreen.js
@@ -1,17 +1,46 @@
 import React from "react";
-import { TextInput, View, StyleSheet } from 'react-native';
+import { TextInput, View, StyleSheet, Dimensions, Image } from 'react-native';
 import { Ionicons } from 'react-native-vector-icons';
 import { IconButton } from 'react-native-paper';
+import Carousel from 'react-native-reanimated-carousel';
 
 function HomeScreen() {
+    const widht = Dimensions.get('window').width;
+    const dataCarousel = [
+      {
+        id: 1,
+        image: 'https://picsum.photos/seed/1/200/300',
+      },
+      {
+        id: 2,
+        image: 'https://picsum.photos/seed/2/200/300',
+      }
+    ];
     return (
-      <View style={styles.container}>
-        <View style={[styles.searchBar, styles.borderShadow]}>
-          <Ionicons name="search" size={12} color="black" />
-          <TextInput placeholder="search"></TextInput>
-        </View>
+      <>
+        <View style={styles.container}>
+          <View style={[styles.searchBar, styles.borderShadow]}>
+            <Ionicons name="search" size={12} color="black" />
+            <TextInput placeholder="search"></TextInput>
+          </View>
           <IconButton icon="menu"/>
-      </View>
+        </View>
+        <View style={{ flex: 1, alignItems: 'center' }}>
+          <Carousel
+            loop
+            width={widht -30}
+            height={200}
+            autoPlay
+            data={dataCarousel}
+            scrollAnimationDuration={1000}
+            renderItem={({ index }) => (
+              <Image
+                style={styles.imageCarousel}
+                source={{ uri: dataCarousel[index].image }} />
+            )}
+          />
+        </View>
+      </>
     )
 }
 
@@ -41,6 +70,11 @@ const styles = StyleSheet.create({
     borderTopWidth: 0,
     borderLeftWidth: 0.2,
     backgroundColor: 'white',
+  },
+  imageCarousel: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
   }
 });
 
