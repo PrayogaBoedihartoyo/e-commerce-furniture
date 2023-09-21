dbaaaaa3c5903813c94ec4fe85fa2e8cd268c074
Prayoga Volantis <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2023-09-21 16:20:43 +0700

    Initial Commit

--- src/views/home/DiscoverScreen.js
+++ src/views/home/DiscoverScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View, StyleSheet, Dimensions, Text, Linking, Platform } from 'react-native';
-import MapView from 'react-native-maps';
+import { View, StyleSheet, Dimensions, Text, Linking, Platform, Animated } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 import Carousel from 'react-native-reanimated-carousel';
 import { Card, IconButton } from 'react-native-paper';
 import { Ionicons } from 'react-native-vector-icons';
 const widht = Dimensions.get('window').width;
+const markerImage = require('../../../assets/icons8-marker-48.png');
+
 const dataCarousel = [
   {
     coordinate: { latitude: 37.8025259, longitude: -122.4351431 },
@@ -14,10 +16,7 @@ const dataCarousel = [
       'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
   },
   {
-    coordinate: {
-      latitude: 37.7896386,
-      longitude: -122.421646,
-    },
+    coordinate: { latitude: 37.7896386, longitude: -122.421646, },
     title: 'Wekea Drop Store 2',
     address: '1234 Main St, San Francisco, CA 94122',
     image:
@@ -32,19 +31,75 @@ const dataCarousel = [
   },
 ];
 export default function DiscoverScreen() {
+  const scrollCarousel = React.useRef(null);
+  const mapAnimation = new Animated.Value(0);
+  const mapRef = React.useRef(null);
+  const carouselAnimation = new Animated.Value(0);
+  const region = {
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  }
+  const onPressMarker = (mapData) => {
+    const marderId = mapData._targetInst.return.key;
+    scrollCarousel.current.scrollTo({ index: +marderId, animated: true })
+  }
+
+  const interpolations = dataCarousel.map((_, index) => {
+    const inputRange = [
+      [index - 1] * widht,
+      index * widht,
+      [index + 1] * widht
+    ];
+    const scale = mapAnimation.interpolate({
+      inputRange,
+      outputRange: [1, 1.5, 1],
+      extrapolate: 'clamp'
+    });
+    return { scale }
+  })
+  const onHideCarousel = () => {
+    Animated.timing(carouselAnimation, {
+      toValue: 1000,
+      duration: 500,
+      useNativeDriver: true
+    }).start();
+  }
   return (
     <View style={StyleSheet.absoluteFillObject}>
       <MapView
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        style={StyleSheet.absoluteFillObject} />
+        ref={mapRef}
+        initialRegion={region}
+        style={StyleSheet.absoluteFillObject} >
+        {
+          dataCarousel.map((item, index) => (
+            <Marker key={index} coordinate={item.coordinate} onPress={(e) => onPressMarker(e)}>
+              <Animated.Image source={markerImage} style={{
+                width: 30,
+                transform: [
+                  {
+                    scale: interpolations[index].scale
+                  }
+                ]
+              }
+              } />
+            </Marker>
+          ))
+        }
+      </MapView>
+      <IconButton icon="menu" mode="contained" onPress={onHideCarousel}/>
       <View style={{position: 'absolute', top: 100, left: 50}} />
-      <View style={{ alignItems: 'center' }}>
+      <Animated.View style={{
+        alignItems: 'center',
+        transform: [
+          {
+            translateX: carouselAnimation
+          },
+        ],
+      }}>
         <Carousel
+          ref={scrollCarousel}
           loop
           width={widht -30}
           height={250}
@@ -54,6 +109,17 @@ export default function DiscoverScreen() {
           autoPlay
           data={dataCarousel}
           scrollAnimationDuration={1000}
+          onProgressChange={(progress) => {
+            mapAnimation.setValue(Math.abs(progress));
+          }}
+          onSnapToItem={(index) => {
+            const { coordinate } = dataCarousel[index];
+            mapRef.current.animateToRegion({
+              ...coordinate,
+              latitudeDelta: region.latitudeDelta,
+              longitudeDelta: region.longitudeDelta,
+            })
+          }}
           renderItem={({ item }) => (
             <Card style={{ overflow: 'hidden' }}>
               <Card.Cover source={{ uri: item.image }} style={{ height: 150, borderRadius: 0 }} />
@@ -67,7 +133,7 @@ export default function DiscoverScreen() {
             </Card>
           )}
         />
-      </View>
+      </Animated.View>
     </View>
   );
 }
